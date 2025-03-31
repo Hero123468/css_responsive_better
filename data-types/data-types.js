c565c4e8b17b2194d14b20bb177cd3a4b9b7ebd1
Jesse Falleur <jessefalleur@gmail.com>
2025-03-31 15:57:53 -0500

    updated launch.json and js files for debugging

---
+++ data-types/data-types.js
@@ -0,0 +1,22 @@
+let codeoutput = document.getElementById('code-output');
+console.log('The output is ,code-output');
+
+let Hello = "Hello World!";
+console.log('This data type is a', typeof Hello);
+
+let number = 3 
+console.log('This data type is a', typeof number);
+
+let bool = false;
+console.log('This is a', typeof bool);
+
+let nul; 
+console.log('This is a',typeof nul);
+
+let obj = {"name" : "Jesse" }
+console.log('This is a', typeof obj);
+
+let myArr = [{"name" : "Falleur"},"name" : "Jesse"}]
+console.log("This is a", typeof myArr)
+
+codeoutput.innerHTML = 'check in Console'
