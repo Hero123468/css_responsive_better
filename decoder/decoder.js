30428dbb0dde4432f4207d97cfbf19b665d10e82
Jesse Falleur <jessefalleur@gmail.com>
2025-04-02 19:04:10 -0500

    modified decoder 2

--- decoder/decoder.js
+++ decoder/decoder.js
@@ -2,30 +2,37 @@
 let input1 = document.getElementById("word");
 let button = document.getElementById("button");
 let output1 = document.getElementById("code-output");
-
-console.log(button, " is the button.");
+console.log(submit, " is the button.");
 
 // defines the function
-function encoder1(){
-    let value = input1.value;
+function encoder1() {
+    let value_= input1.value;
+    let value = value.toUpperCase()
     console.log("INPUT IS: ", value);
 
+    let encoded = ""
+    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+    for (let i = 0; i < 9; i++) {
+        console.log("the letter is", i)
+        let encoded = alphabet.indexOf(value[i])   
+     }
 
     let len = value.length;
     console.log(" The word is ", len, " letters long.");
 
     let encodedValue = "";
 
-    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
     for(let i=0;i<len; i++) {
         console.log("the letter is ", value[i]);
         //encryption algorithm
         //convert letters to numbers
         let result = alphabet.indexOf(value[i]);
-        encodedValue += result +"."; 
+        
+        
+        encoded += result; 
     }
-    output1.innerHTML = encodedValue; 
+    output1.innerHTML = encoded; 
 }
 
 //function call
